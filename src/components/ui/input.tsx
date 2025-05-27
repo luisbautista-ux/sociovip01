4857fb6728f3b9fb5d4e40e6b4d51c626bf9e51a
Luis Miguel Felix Atuncar <luisfelix1983@gmail.com>
2025-05-27 14:51:43 +0000

    los placeholder del formulario colocarlo en letra un poco màs pequeña y estilo cursiva con fondo blanco

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -8,9 +9,8 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed",
-          disabled ? "bg-muted/50 text-muted-foreground/80" : "", // Diferenciar fondo y texto para deshabilitados con valor
-          // "disabled:opacity-50", // Se comenta o ajusta para que el texto del valor sea legible
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground placeholder:text-sm placeholder:italic focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed",
+          disabled ? "bg-muted/50 text-muted-foreground/80" : "",
           className
         )}
         ref={ref}
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,3 +1,4 @@
+
 import * as React from 'react';
 
 import {cn} from '@/lib/utils';
@@ -7,9 +8,8 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
     return (
       <textarea
         className={cn(
-          'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed',
-          disabled ? "bg-muted/50 text-muted-foreground/80" : "", // Diferenciar fondo y texto para deshabilitados con valor
-          // 'disabled:opacity-50', // Se comenta o ajusta para que el texto del valor sea legible
+          'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground placeholder:text-sm placeholder:italic focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed',
+          disabled ? "bg-muted/50 text-muted-foreground/80" : "",
           className
         )}
         ref={ref}
