1237c1bc5c38adadcecf1eb44bf08ff85cdccaf8
Luis Bautista <luis.bautista@uai.edu.pe>
2025-08-28 15:33:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,20 +4,23 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAuth, type Auth } from "firebase/auth";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 
-// --- INSTRUCCIONES IMPORTANTES ---
-// REEMPLAZA los valores de marcador de posición de abajo con los de tu proyecto de Firebase.
-// Ve a tu consola de Firebase -> Configuración del proyecto -> Tus apps -> SDK de Firebase -> Configuración.
 const firebaseConfig = {
   apiKey: "AIzaSyDtMgj_WySsrpmVdYLxem22UzCG8xCKQ4I",
   authDomain: "cloverpass.firebaseapp.com",
   projectId: "cloverpass",
   storageBucket: "cloverpass.appspot.com",
   messagingSenderId: "564817412003",
-  appId: "1:564817412003:web:8938e8372bb360c0bde416"
+  appId: "1:564817412003:web:83938e8372bb360c0bde416"
 };
 
 // Initialize Firebase for SSR
-const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app: FirebaseApp;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
 const db: Firestore = getFirestore(app);
 const auth: Auth = getAuth(app);
 const storage: FirebaseStorage = getStorage(app);
