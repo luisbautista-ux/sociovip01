94408bc25fae2b3b4ae2f4db6bf7251c8ac1e262
Luis Bautista <luis.bautista@uai.edu.pe>
2025-12-30 15:43:33 +0000

    sigue saliendo el error revisa ese file(segunda imagen})

--- src/app/api/redeem-code/route.ts
+++ src/app/api/redeem-code/route.ts
@@ -1,7 +1,7 @@
 
 import {NextResponse} from 'next/server';
 import {z} from 'zod';
-import {admin, adminDb} from '@/lib/firebase/firebaseAdmin';
+import { adminDb, admin } from '@/lib/firebase/firebaseAdmin';
 import {anyToDate, isEntityCurrentlyActivatable} from '@/lib/utils';
 import type {BusinessManagedEntity, GeneratedCode, QrClient} from '@/lib/types';
 
