49887218a7be7258b3ae329c56d31139cd9d50e9
luisbautistaq <bautistaluis989@gmail.com>
2026-05-18 16:20:15 -0500

    2.1

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -1,14 +1,19 @@
 // src/lib/firebase/firebaseAdmin.ts
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
 
-const serviceAccountCredentials = {
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-};
-
-// Evitar reinicialización en desarrollo
 if (!admin.apps.length) {
-    admin.initializeApp(serviceAccountCredentials);
+    // Si estamos en tu computadora local (modo desarrollo)
+    if (process.env.NODE_ENV === 'development') {
+        // Usamos require en vez de import para que la nube no se bloquee buscándolo
+        const serviceAccount = require('./serviceAccountKey.json');
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
+    }
+    // Si estamos en la nube (producción)
+    else {
+        admin.initializeApp(); // Usa la auto-autenticación de Firebase
+    }
 }
 
 export const adminDb = admin.firestore();
